0ed05745e3e99d5a67c99d5b31646cc4602c6612
Martin Siddons <gny17hvu@uea.ac.uk>
2018-01-07 03:25:19 +0000

    Completed all questions.

--- Lab6/Source.cpp
+++ Lab6/Source.cpp
@@ -56,7 +56,7 @@ int next_visitor(vector<bool> & occupied)
 
 void draw_stalls(vector<bool> const & occupied)
 {
-	for (int i = 0; i < occupied.size(); i++)
+	for (unsigned i = 0; i < occupied.size(); i++)
 		cout << (occupied[i] ? "X" : "_");
 	cout << endl;
 }
@@ -67,7 +67,7 @@ void exercise1()
 	cout << "Number of urinals: ";
 	cin >> urinalNum;
 	vector<bool> urinals(urinalNum, false);
-	for (int i = 0; i < urinals.size(); i++)
+	for (unsigned i = 0; i < urinals.size(); i++)
 	{
 		cout << "Next position is number: " << next_visitor(urinals) << endl;
 		draw_stalls(urinals);
@@ -79,7 +79,7 @@ void exercise1()
 
 void draw_piles(vector<int> const & pile)
 {
-	for (int i = 0; i < pile.size(); i++)
+	for (unsigned i = 0; i < pile.size(); i++)
 		cout << pile[i] << ", ";
 	cout << endl;
 }
@@ -87,7 +87,7 @@ void draw_piles(vector<int> const & pile)
 void form_new_pile(vector<int> & pile)
 {
 	bool removed = 0;
-	for (int i = 0; i < pile.size(); i++)
+	for (unsigned i = 0; i < pile.size(); i++)
 	{
 		pile[i]--;
 		if (pile[i] == 0)
@@ -96,33 +96,34 @@ void form_new_pile(vector<int> & pile)
 	pile.push_back(pile.size());
 
 	if (removed != 0)
-		for (int i = 0; i < pile.size(); i++)
+		for (unsigned i = 0; i < pile.size(); i++)
 		{
 			if (pile[i] == 0)
+			{
 				pile.erase(pile.begin() + i);
+				i--;
+			}
 		}
 }
 
 int complete_check(vector<int> pile)
 {
-	int max = 0;
-	for (int i = 0; i < pile.size(); i++)
-		if (pile[i] > max)
-			max = pile[i];
-	if (max > 9)
+	if (pile.size() != 9)
 		return 1;
 
 	vector<int> run;
 	for (int i = 1; i < 10; i++)
 		run.push_back(i);
-	for (int i = 0; i < pile.size(); i++)
-		for (int j = 0; j < run.size(); j++)
+	for (unsigned i = 0; i < pile.size(); i++)
+		for (unsigned j = 0; j < run.size(); j++)
 		{
 			if (pile[i] == run[j])
 				run.erase(run.begin() + j);
 		}
-	if (run.size() == 0) // shit hit the fan somewhere here fuck it idk
+	if (run.size() == 0)
 		return 0;
+	else
+		return 1;
 }
 
 void exercise2()
@@ -146,44 +147,151 @@ void exercise2()
 
 //Q3------------------------------------------------------------------------------------------------
 
+vector<int> vectorCreate(minstd_rand0 & g)
+{
+	vector<int> a;
+	int count = (g() % 8 + 3);
+	for (int i = 0; i < count; i++)
+		a.push_back(g() % 30 + 1);
+	return a;
+}
+
+vector<int> mergeSorted(vector<int> const & a, vector<int> const & b)
+{
+	vector<int> merged;
+	for (int i = 1; i <= 30; i++)
+	{
+		for (unsigned j = 0; j < a.size(); j++)
+		{
+			if (a[j] == i)
+				merged.push_back(a[j]);
+		}
+		for (unsigned j = 0; j < b.size(); j++)
+		{
+			if (b[j] == i)
+				merged.push_back(b[j]);
+		}
+	}
+	return merged;
+}
+
+void vectorPrint(vector<int> const & a)
+{
+	for (unsigned i = 0; i < a.size(); i++)
+		cout << a[i] << " ";
+	cout << endl;
+}
 
 void exercise3()
 {
-
+	minstd_rand0 g((unsigned)time(0));
+	vector<int> vectorA = vectorCreate(g);
+	vector<int> vectorB = vectorCreate(g);
+	vector<int> vectorC = mergeSorted(vectorA, vectorB);
+	vectorPrint(vectorA);
+	vectorPrint(vectorB);
+	vectorPrint(vectorC);
+	cout << endl;
 }
 
 //Q4------------------------------------------------------------------------------------------------
 
+void trickySwap(int & a, int & b)
+{
+	a = a - b;
+	b = a + b;
+	a = b - a;
+}
 
 void exercise4()
 {
-
+	int a, b;
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	trickySwap(a, b);
+	cout << "When trickyswapped, the first number is " << a << ", and the second is " << b << endl << endl;
 }
 
 //Q5------------------------------------------------------------------------------------------------
 
+void rollDice(int *r)
+{
+	int a, *p = r - 2;
+	for (int i = 1; i <= 360000; i++) // generate 360000 throws of 2 dice.
+	{
+		a = (rand() % 6 + 1) + (rand() % 6 + 1); // generate a random number between 1-6 and add it to another random number from the same range, then assign that to 'a'.
+		for (int c = 2; c <= 12; c++) // loop which checks a (c)ounter and adds that result to the correct spot.
+			if (a == c)
+				p[c]++;
+	}
+}
 
 void exercise5()
 {
-
+	int rolls[11]{ 0,0,0,0,0,0,0,0,0,0,0 }; // initialise the vector with 11 elements since that's all we're using.
+	srand(time(0));
+	rollDice(rolls);
+	for (int i = 0; i <= 10; i++) // loop which iterates through numbers 2-12 to print the row numbers.
+	{
+		cout << (i + 2) << ": " << rolls[i] << endl;
+		//cout << setw(2) << setfill(' ') << i << ":  "; // print the line up until the stars.
+		//cout << setw(lrint(rolls[i] / 10000.0) + 1) << setfill('*') << " " << endl; // cleverly fill each row of stars using setw rather than another for loop.
+	}
 }
 
 //Q6------------------------------------------------------------------------------------------------
 
+bool isMember(char c, string delim)
+{
+	return (delim.find(c, 0) != string::npos);
+}
+
+string getNext(string const & toExtract, int & pos, string const & delim)
+{
+	string result;
+	while (pos < toExtract.size() && isMember(toExtract[pos], delim))
+		pos++; // if the first characters are in delim, skip and move to the next.
+	while (pos < toExtract.size() && !isMember(toExtract[pos], delim))
+	{
+		result.push_back(toExtract[pos]);
+		pos++;
+	}
+	return result;
+}
+
+void processLine(string const & line)
+{
+	int pos = 0;
+	cout << "CODE:       " << getNext(line, pos, ",") << endl;
+	cout << "TITLE:      " << getNext(line, pos, ",") << endl;
+	cout << "CREDIT:     " << getNext(line, pos, ",") << endl;
+	cout << "ASSESSMENT: ";
+	while (pos < line.size())
+	{
+		cout << getNext(line, pos, ",") << ":";
+		cout << getNext(line, pos, ",") << " ";
+	}
+	cout << endl;
+}
 
 void exercise6()
 {
-
+	processLine("CMP-0002A,FOUNDATIONS OF COMPUTING,20,CWK,50,CT,50");
+	cout << endl << endl;
+	processLine("CMP-0005A,INTRODUCTORY PROGRAMMING,20,CWK,25,CWK,25,EXAM,50");
+	cout << endl;
 }
 
 
 //Menu------------------------------------------------------------------------------------------------
 int main()
 {
-	srand(time(0));
 	int exercise = -1;
 	while (exercise != 0)
 	{
+		srand((unsigned)time(0));
 		cout << "Select an exercise number (1-6) or 0 to exit: ";
 		cin >> exercise;
 		switch (exercise)
